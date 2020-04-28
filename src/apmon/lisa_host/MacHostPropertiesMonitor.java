82e4f226386f8466411d2086ac833e149e7f3543
Costin Grigoras <costing@gmail.com>
2020-04-28 14:41:32 +0200

    Adapt to the new command line parameters and output format of `top` on Mac

--- src/apmon/lisa_host/MacHostPropertiesMonitor.java
+++ src/apmon/lisa_host/MacHostPropertiesMonitor.java
@@ -2,12 +2,13 @@ package apmon.lisa_host;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * @author Gregory Denis
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
+ *         TODO To change the template for this generated type comment go to Window -
+ *         Preferences - Java - Code Style - Code Templates
  */
 /**
  * @author costing
@@ -42,63 +43,59 @@ public class MacHostPropertiesMonitor {
 	private String command = "";
 	private cmdExec execute = null;
 	private String sep = null;
-    private Parser parser = null;
-    private Hashtable<String, Integer> netSockets = null;
-    private Hashtable<String, Integer> netTcpDetails = null;
-	
+	private Parser parser = null;
+	private Hashtable<String, Integer> netSockets = null;
+	private Hashtable<String, Integer> netTcpDetails = null;
+
 	/**
 	 * 
 	 */
 	public MacHostPropertiesMonitor() {
 
 		parser = new Parser();
-        execute = new cmdExec();
+		execute = new cmdExec();
 		execute.setTimeout(3 * 1000);
 		sep = System.getProperty("file.separator");
 		// get the network interfaces up
-		command = sep + "sbin" + sep	+ "ifconfig -l -u";
+		command = sep + "sbin" + sep + "ifconfig -l -u";
 		String result = execute.executeCommand(command, "lo0");
-		//System.out.println(command + " = "+ result);
+		// System.out.println(command + " = "+ result);
 		if (result == null || result.equals("")) {
 			System.out.println(command + ": No result???");
-		} else {
+		}
+		else {
 			int where = result.indexOf("lo0");
-			networkInterfaces =
-				result
-					.substring(where + 3, result.length())
-					.replaceAll("  ", " ")
-					.trim()
-					.split(
-					" ");
+			networkInterfaces = result.substring(where + 3, result.length()).replaceAll("  ", " ").trim().split(" ");
 
-			//			 get the currently used Mac Address
+			// get the currently used Mac Address
 			for (int i = 0; i < networkInterfaces.length; i++) {
 				String current = networkInterfaces[i];
 				command = sep + "sbin" + sep + "ifconfig " + current;
 				result = execute.executeCommand(command, current);
-				//System.out.println(command + " = " + result);
+				// System.out.println(command + " = " + result);
 				if (result == null || result.equals("")) {
 					System.out.println(command + ": No result???");
-				} else {
+				}
+				else {
 					if (result.indexOf("inet ") != -1) {
 						int pointI = result.indexOf("ether");
 						int pointJ = result.indexOf("media", pointI);
-						macAddress =
-							result.substring(pointI + 5, pointJ).trim();
-						//System.out.println("Mac Address:" + macAddress);
+						macAddress = result.substring(pointI + 5, pointJ).trim();
+						// System.out.println("Mac Address:" + macAddress);
 						activeInterface = current;
 					}
 				}
 			}
 		}
 
-		//		 get the disk information
-		command = sep + "bin" + sep + "df -k -h "	+ sep;
+		// get the disk information
+		command = sep + "bin" + sep + "df -k -h " + sep;
 		result = execute.executeCommand(command, "/dev");
-		//System.out.println(command + " = "+ result);
+		// System.out.println(command + " = "+ result);
 		if (result == null || result.equals("")) {
 			System.out.println(command + ": No result???");
-		} else {
+		}
+		else {
 			parseDf(result);
 		}
 
@@ -123,20 +120,20 @@ public class MacHostPropertiesMonitor {
 		}
 
 		// get CPU, load, Mem, Pages, Processes from '/usr/bin/top'
-		command = sep + "usr" + sep + "bin" + sep
-				+ "top -d -l2 -n1 -F -R -X";
+		command = sep + "usr" + sep + "bin" + sep + "top -d -l2 -n1 -F -R";
 		String result = execute.executeCommand(command, "PID", 2);
-		//System.out.println(command + " = "+ result);
+		// System.out.println(command + " = "+ result);
 		if (result == null || result.equals("")) {
 			System.out.println("No result???");
-		} else {
+		}
+		else {
 			parseTop(result);
 		}
 	}
 
 	private void parseDf(String toParse) {
 
-		//System.out.println("result of df -k -h /:" + toParse);
+		// System.out.println("result of df -k -h /:" + toParse);
 
 		int pointI = toParse.indexOf("/dev/");
 		int pointJ = 0;
@@ -147,7 +144,8 @@ public class MacHostPropertiesMonitor {
 			pointJ = toParse.indexOf(" ", pointI);
 			pointK = indexOfUnitLetter(toParse, pointJ);
 			diskTotal = toParse.substring(pointJ, pointK).trim();
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
@@ -156,7 +154,8 @@ public class MacHostPropertiesMonitor {
 			pointI = toParse.indexOf(" ", pointK);
 			pointJ = indexOfUnitLetter(toParse, pointI);
 			diskUsed = toParse.substring(pointI, pointJ).trim();
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
@@ -165,20 +164,23 @@ public class MacHostPropertiesMonitor {
 			pointK = toParse.indexOf(" ", pointJ);
 			pointI = indexOfUnitLetter(toParse, pointK);
 			diskFree = toParse.substring(pointK, pointI).trim();
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
-		/*System.out.println(
-			"Disk: Total:"
-				+ diskTotal
-				+ " Used:"
-				+ diskUsed
-				+ " Free:"
-				+ diskFree);*/
+		/*
+		 * System.out.println(
+		 * "Disk: Total:"
+		 * + diskTotal
+		 * + " Used:"
+		 * + diskUsed
+		 * + " Free:"
+		 * + diskFree);
+		 */
 	}
 
-	private int indexOfUnitLetter(String inside, int from) {
+	private static int indexOfUnitLetter(String inside, int from) {
 
 		int temp = inside.indexOf('K', from);
 		if (temp == -1 || (temp - from > 10)) {
@@ -201,7 +203,7 @@ public class MacHostPropertiesMonitor {
 		return temp;
 	}
 
-	private int lastIndexOfUnitLetter(String inside, int from) {
+	private static int lastIndexOfUnitLetter(String inside, int from) {
 
 		int temp = inside.lastIndexOf('K', from);
 		if (temp == -1 || (from - temp > 10)) {
@@ -224,65 +226,75 @@ public class MacHostPropertiesMonitor {
 		return temp;
 	}
 
-	private double howMuchMegaBytes(char a) {
+	private static double howMuchMegaBytes(char a) {
 
 		switch (a) {
-			case 'T' :
-				return 1048576.0;
-			case 'G' :
-				return 1024.0;
-			case 'M' :
-				return 1.0;
-			case 'K' :
-				return 0.0009765625;
-			case 'B' :
-				return 0.0000009537;
-			default :
-				return 1.0;
+		case 'T':
+			return 1048576.0;
+		case 'G':
+			return 1024.0;
+		case 'M':
+			return 1.0;
+		case 'K':
+			return 0.0009765625;
+		case 'B':
+			return 0.0000009537;
+		default:
+			return 1.0;
 		}
 	}
-	
+
+	private static final String TOP_PROCESSES = "Processes:";
+	private static final String TOP_LOAD_AVG = "Load Avg:";
+	private static final String TOP_CPU_USAGE = "CPU usage:";
+	private static final String TOP_PHYS_MEM = "PhysMem:";
+	private static final String TOP_VM = "VM:";
+
 	private void parseTop(String toParse) {
 
-		//System.out.println("\n******\n"+toParse+"\n********\n");
+		// System.out.println("\n******\n"+toParse+"\n********\n");
 
 		int pointA = 0;
 		int pointB = 0;
 		int unitPos = 0;
 		double sum = 0.0;
 
-		//		 Get number of total Processes
+		// Get number of total Processes
+		// Processes: 209 total, 13 running, 196 sleeping, 661 threads
 		try {
-			pointA = toParse.indexOf("Procs:");
-			//System.out.println("First Procs at " + pointA);
-			pointA = toParse.indexOf("Procs:", pointA + 6) + 6;
-			//System.out.println("Second Procs at " + pointA);
+			pointA = toParse.indexOf(TOP_PROCESSES);
+			// System.out.println("First Procs at " + pointA);
+			pointA = toParse.indexOf(TOP_PROCESSES, pointA + TOP_PROCESSES.length()) + TOP_PROCESSES.length();
+			// System.out.println("Second Procs at " + pointA);
 			pointB = toParse.indexOf(",", pointA + 1);
 			nbProcesses = toParse.substring(pointA, pointB).trim();
-			//System.out.println(nbProcesses + " processes");
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+			// System.out.println(nbProcesses + " processes");
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
 		// Get the loads...
+		// Load Avg: 12.81, 11.31, 11.34
 		try {
-			pointA = toParse.indexOf("LoadAvg:", pointA);
-			pointA += 9;
+			pointA = toParse.indexOf(TOP_LOAD_AVG, pointA);
+			pointA += TOP_LOAD_AVG.length() + 1;
 			pointB = toParse.indexOf(",", pointA);
 			load1 = toParse.substring(pointA, pointB).trim();
 			pointA = toParse.indexOf(",", pointB + 1);
 			load5 = toParse.substring(pointB + 1, pointA).trim();
-			pointB = toParse.indexOf("CPU:", pointA + 1);
-			pointB = toParse.lastIndexOf(".", pointB);
+			pointB = toParse.indexOf("\n", pointA);
 			load15 = toParse.substring(pointA + 1, pointB).trim();
-			//System.out.println("load: [" + load1 + "][" + load5 + "][" + load15 + "]");
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+			// System.out.println("load: [" + load1 + "][" + load5 + "][" + load15 + "]");
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
 		// Get CPUs...
+		// CPU usage: 87.87% user, 12.12% sys, 0.0% idle
 		try {
-			pointB = toParse.indexOf("CPU:", pointB + 1) + 4;
+			pointB = toParse.indexOf(TOP_CPU_USAGE, pointB + 1) + TOP_CPU_USAGE.length();
 			pointA = toParse.indexOf("% user", pointB);
 			cpuUSR = toParse.substring(pointB, pointA).trim();
 			pointA = toParse.indexOf(",", pointA);
@@ -293,125 +305,153 @@ public class MacHostPropertiesMonitor {
 			cpuIDLE = toParse.substring(pointA + 1, pointB).trim();
 			sum = 100.0 - Double.parseDouble(cpuIDLE);
 			cpuUsage = String.valueOf(sum);
-			/*System.out.println("Cpu Usage:" + cpuUsage + " user:"
-					+ cpuUSR + " sys:" + cpuSYS
-					+ " idle:" + cpuIDLE);*/
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
-			//ignore
+			/*
+			 * System.out.println("Cpu Usage:" + cpuUsage + " user:"
+			 * + cpuUSR + " sys:" + cpuSYS
+			 * + " idle:" + cpuIDLE);
+			 */
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
+			// ignore
 		}
 
-		//		 Get Mem...
+		// Get Mem...
+		// PhysMem: 5890M used (1023M wired), 2300M unused.
 		try {
-			pointA = toParse.indexOf("PhysMem", pointB);
-			pointA += 8;
+			pointA = toParse.indexOf(TOP_PHYS_MEM, pointB);
+			pointA += TOP_PHYS_MEM.length();
 			pointB = toParse.indexOf("M used", pointA);
-			pointA = toParse.lastIndexOf(",", pointB);
 			memUsed = toParse.substring(pointA + 1, pointB).trim();
-			pointB = toParse.indexOf("M free", pointB);
+			pointB = toParse.indexOf("M unused", pointB);
 			pointA = toParse.lastIndexOf(",", pointB);
 			memFree = toParse.substring(pointA + 1, pointB).trim();
-			//System.out.println("Mem Used:"+memUsed+"M Free:"+memFree+"M");
+			// System.out.println("Mem Used:"+memUsed+"M Free:"+memFree+"M");
 			sum = Double.parseDouble(memUsed) + Double.parseDouble(memFree);
-			double percentage = Integer.parseInt(memUsed) / sum * 100;
+			double percentage = Integer.parseInt(memUsed) * 100 / sum;
 			memUsage = String.valueOf(percentage);
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 
 		// Pages In/Out...
+		// VM: 946G vsize, 0B framework vsize, 0(0) swapins, 0(0) swapouts.
 		try {
-			pointA = toParse.indexOf("VirtMem:", pointB + 6);
-			pointB = toParse.indexOf("pagein", pointA);
+			pointA = toParse.indexOf(TOP_VM, pointB + 6);
+			pointB = toParse.indexOf("swapins", pointA);
 			pointA = toParse.lastIndexOf(",", pointB);
 			pagesIn = toParse.substring(pointA + 1, pointB).trim();
-			pointA = toParse.indexOf("pageout", pointB);
+
+			if (pagesIn.indexOf('(') > 0)
+				pagesIn = pagesIn.substring(0, pagesIn.indexOf('('));
+
+			pointA = toParse.indexOf("swapouts", pointB);
 			pointB = toParse.lastIndexOf(",", pointA);
 			pagesOut = toParse.substring(pointB + 1, pointA).trim();
-			//System.out.println("Pages In:" + pagesIn + " Out" + pagesOut);
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+
+			if (pagesOut.indexOf('(') > 0)
+				pagesOut = pagesOut.substring(0, pagesOut.indexOf('('));
+
+			// System.out.println("Pages In:" + pagesIn + " Out" + pagesOut);
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			System.out.println("Can't find pages in :" + toParse);
 		}
 
 		// Get Network IO...
+		// Networks: packets: 2/172B in, 1/732B out.
 		try {
 			pointA = toParse.indexOf("Networks:", pointB) + 9;
-			pointB = toParse.indexOf("data =", pointA) + 6;
+			pointB = toParse.indexOf("packets: ", pointA) + 9;
 			pointA = toParse.indexOf("in", pointB);
 			unitPos = lastIndexOfUnitLetter(toParse, pointA);
 			netIn = toParse.substring(pointB, unitPos).trim();
-			//System.out.print("Net In:" + netIn);
-			double factor =
-				howMuchMegaBytes(
-					(toParse.substring(unitPos, unitPos + 1).toCharArray())[0]);
+
+			if (netIn.indexOf('/') >= 0)
+				netIn = netIn.substring(netIn.indexOf('/') + 1);
+
+			// System.out.print("Net In:" + netIn);
+			double factor = howMuchMegaBytes((toParse.substring(unitPos, unitPos + 1).toCharArray())[0]);
 			netIn = String.valueOf(Double.parseDouble(netIn) * factor * 4);
 			pointB = toParse.indexOf("out", pointA);
 			unitPos = lastIndexOfUnitLetter(toParse, pointB);
-			factor =
-				howMuchMegaBytes(
-					(toParse.substring(unitPos, unitPos + 1).toCharArray())[0]);
+			factor = howMuchMegaBytes((toParse.substring(unitPos, unitPos + 1).toCharArray())[0]);
 			netOut = toParse.substring(pointA + 3, unitPos).trim();
-			//System.out.println("Net Out:" + netOut);
+
+			if (netOut.indexOf('/') >= 0)
+				netOut = netIn.substring(netOut.indexOf('/') + 1);
+
+			// System.out.println("Net Out:" + netOut);
 			netOut = String.valueOf(Double.parseDouble(netOut) * factor);
-			//System.out.println("Network In:" + netIn + " OUT:" + netOut);
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+			// System.out.println("Network In:" + netIn + " OUT:" + netOut);
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			System.out.println(e);
 		}
 
 		// Get Disks IO...
+		// Disks: 0/0B read, 4/2172K written.
 		try {
-			pointB = toParse.indexOf("Disks:", pointA) + 6;
-			pointA = toParse.indexOf("data =", pointB) + 6;
-			pointB = toParse.indexOf("in,", pointA);
+			pointA = toParse.indexOf("Disks:", pointA) + 6;
+			pointB = toParse.indexOf("read,", pointA);
 			unitPos = lastIndexOfUnitLetter(toParse, pointB);
 			diskIn = toParse.substring(pointA, unitPos).trim();
-			pointA = toParse.indexOf("out", pointB);
+
+			if (diskIn.indexOf('/') >= 0)
+				diskIn = diskIn.substring(diskIn.indexOf('/') + 1);
+
+			pointA = toParse.indexOf("written", pointB);
 			unitPos = lastIndexOfUnitLetter(toParse, pointA);
 			diskOut = toParse.substring(pointB + 3, unitPos).trim();
 
-			//System.out.println("diskIO In:" + diskIn + " Out:" + diskOut);
+			if (diskOut.indexOf('/') >= 0)
+				diskOut = diskOut.substring(diskOut.indexOf('/') + 1);
+
+			// System.out.println("diskIO In:" + diskIn + " Out:" + diskOut);
 			diskIO = diskIn + diskOut;
-		} catch (java.lang.StringIndexOutOfBoundsException e) {
+		}
+		catch (java.lang.StringIndexOutOfBoundsException e) {
 			// ignore
 		}
 	}
-    
-    /**
-     * @return processes states
-     */
-    public Hashtable<String, Integer> getProcessesState() {
-        Hashtable<String, Integer> states = null;
-        String output = execute.executeCommandReality("ps -e -A -o state", "");
-        if (output != null && !output.equals("")) {
-            
-            parser.parse(output);
-            String line = parser.nextLine();
-            
-            states = new Hashtable<String, Integer>();
-            states.put("D", Integer.valueOf(0));
-            states.put("R", Integer.valueOf(0));
-            states.put("S", Integer.valueOf(0));
-            states.put("T", Integer.valueOf(0));
-            states.put("Z", Integer.valueOf(0));
-    
-            while (line != null) {
-                if ((line.startsWith(" ") || line.startsWith("\t"))) {
-                    line = parser.nextLine();
-                    continue;
-                }
-                Enumeration<String> e = states.keys();
-                while(e.hasMoreElements()){
-                    String key = e.nextElement();
-                    if(line.startsWith(key)){
-                        int x = (states.get(key)).intValue();
-                        x++;
-                        states.put(key, Integer.valueOf(x));
-                    }
-                }
-                line = parser.nextLine();
-            }
-        }
-        return states;
-    }
+
+	/**
+	 * @return processes states
+	 */
+	public Hashtable<String, Integer> getProcessesState() {
+		Hashtable<String, Integer> states = null;
+		String output = execute.executeCommandReality("ps -e -A -o state", "");
+		if (output != null && !output.equals("")) {
+
+			parser.parse(output);
+			String line = parser.nextLine();
+
+			states = new Hashtable<>();
+			states.put("D", Integer.valueOf(0));
+			states.put("R", Integer.valueOf(0));
+			states.put("S", Integer.valueOf(0));
+			states.put("T", Integer.valueOf(0));
+			states.put("Z", Integer.valueOf(0));
+
+			while (line != null) {
+				if ((line.startsWith(" ") || line.startsWith("\t"))) {
+					line = parser.nextLine();
+					continue;
+				}
+				Enumeration<String> e = states.keys();
+				while (e.hasMoreElements()) {
+					String key = e.nextElement();
+					if (line.startsWith(key)) {
+						int x = (states.get(key)).intValue();
+						x++;
+						states.put(key, Integer.valueOf(x));
+					}
+				}
+				line = parser.nextLine();
+			}
+		}
+		return states;
+	}
 
 	/**
 	 * @return cpu usage
@@ -517,20 +557,20 @@ public class MacHostPropertiesMonitor {
 	public String getNoProcesses() {
 		return nbProcesses;
 	}
-    
-    /**
-     * @return sockets
-     */
-    public Hashtable<String, Integer> getNetSockets() {
-        return netSockets;
-    }
-    
-    /**
-     * @return tcp states
-     */
-    public Hashtable<String, Integer> getTcpDetails() {
-        return netTcpDetails;
-    }
+
+	/**
+	 * @return sockets
+	 */
+	public Hashtable<String, Integer> getNetSockets() {
+		return netSockets;
+	}
+
+	/**
+	 * @return tcp states
+	 */
+	public Hashtable<String, Integer> getTcpDetails() {
+		return netTcpDetails;
+	}
 
 	/**
 	 * @return load 1
@@ -578,5 +618,18 @@ public class MacHostPropertiesMonitor {
 		if (ifName.equalsIgnoreCase(activeInterface))
 			return netOut;
 		return "0";
+	}
+
+	/**
+	 * Debug method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MacHostPropertiesMonitor monitor = new MacHostPropertiesMonitor();
+		Map<String, Integer> state = monitor.getProcessesState();
+
+		for (Map.Entry<String, Integer> entry : state.entrySet())
+			System.err.println(entry.getKey() + " : " + entry.getValue());
 	}
 }
